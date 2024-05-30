77d40fbad095a3a0a9e7229ac87452b9c9f3a5fc
nayevsk <nayevskau@gmail.com>
2024-05-30 13:31:47 +1000

    Bin2Hex Completed

--- function.js
+++ function.js
@@ -106,7 +106,7 @@ function Bin2Dec (){
     let WholeBinNumberLengh = 0;
     let WholeDecNumber = 0;
     let FracDecNumber = 0;
-    var SplitBinNumber = BinNumber.split("");
+    var SplitBinNumber = BinNumber.split(""); 
 
     // WholeBin2Dec Sum:
 
@@ -137,28 +137,67 @@ function Bin2Dec (){
 function Bin2Hex(){
 
     let BinNumber = "111011.11111";
-    let SplitBinNumber = Bin2Dec.split("");
+    let SplitBinNumber = BinNumber.split("");
     let HexNumber = "";
-
     let Bin2HexConversion = {
         "0000": 0,
         "0001": 1,
         "0010": 2,
-        "0010": 3,
-        "0011": 4,
-        "0100": 5,
-        "0101": 6,
-        "0110": 7,
-        "0111": 8,
-        "1000": 9,
-        "1001": A,
-        "1010": B,
-        "1011": C,
-        "1100": D,
-        "1101": E,
-        "1111": F
+        "0011": 3,
+        "0100": 4,
+        "0101": 5,
+        "0110": 6,
+        "0111": 7,
+        "1000": 8,
+        "1001": 9,
+        "1010": "A",
+        "1011": "B",
+        "1100": "C",
+        "1101": "D",
+        "1110": "E",
+        "1111": "F"
     }
 
+    // Preparing Bin Number to conversion:
+
+    //      Whole Bin Conversion:
+
+    let BinNumberLengh = BinNumber.length;
+    let WholeBinNumberLengh = SplitBinNumber.findIndex (ele => ele == ".")    // find the index of "." in the array.
+    let Bin2Hex = "";
+
+    while (WholeBinNumberLengh%4 != 0) {        // making sure WholeBinNumber has a lengh multiple of 8 prior to conversion.
+        
+        SplitBinNumber.unshift("0");
+        WholeBinNumberLengh += 1; 
+    }
+    //      Frac Bin Conversion:
+
+    let FracBinNumberLengh = BinNumberLengh - WholeBinNumberLengh +1; // +1 added in order to compensate the lengh used by "."
+
+    while( FracBinNumberLengh%4 != 0 ){
+        SplitBinNumber.push("0");
+        FracBinNumberLengh += 1;
+    }
+
+    // Joining array (BinNumberReady4Convertion):
+
+    BinNumber = SplitBinNumber.join("");
+    let Index = SplitBinNumber.findIndex (ele => ele == "."); // Stores the location of "." in the array (to be added after conversion). 
+    SplitBinNumber.splice(Index,1)
+
+    // Whole Bin2Hex:
+
+    for (i = 0; i < BinNumber.length -1; i += 4) {    // -1 to consider the removal of "." from the array
+        Aux = SplitBinNumber.slice(i, i+4);
+        Bin2Hex += Bin2HexConversion[Aux.join("")];
+    };  
+
+    let Bin2HexSplit = Bin2Hex.split("");              // Adding back the "."
+    Bin2HexSplit.splice(2,0,".");
+    Bin2Hex = Bin2HexSplit.join("");
+
+    return console.log (Bin2Hex);
 
 }
 
---
+++ testScript.js
@@ -0,0 +1,7 @@
+let a = "lucas";
+let aSplit = a.split("");
+
+let index4c = aSplit.findIndex(ele => ele == "c");
+
+console.log (index4c);
+
